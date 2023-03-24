3f37e59717e6dda8e3f27548aa29f4cde4f27cde
Julijan Katic <julijan@katic.io>
2023-03-24 14:19:53 +0100

    Changed result type and fixed some bugs in Subscriber

--- bundles/org.palladiosimulator.analyzer.slingshot.eventdriver/src/org/palladiosimulator/analyzer/slingshot/eventdriver/entity/Subscriber.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.eventdriver/src/org/palladiosimulator/analyzer/slingshot/eventdriver/entity/Subscriber.java
@@ -1,10 +1,10 @@
 package org.palladiosimulator.analyzer.slingshot.eventdriver.entity;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
 
-import org.palladiosimulator.analyzer.slingshot.eventdriver.annotations.Subscribe;
 import org.palladiosimulator.analyzer.slingshot.eventdriver.entity.interceptors.IPostInterceptor;
 import org.palladiosimulator.analyzer.slingshot.eventdriver.entity.interceptors.IPreInterceptor;
 import org.palladiosimulator.analyzer.slingshot.eventdriver.entity.interceptors.InterceptorInformation;
@@ -13,14 +13,33 @@ import org.palladiosimulator.analyzer.slingshot.eventdriver.returntypes.Result;
 
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.functions.Consumer;
-import java.util.Collections;
 
+/**
+ * A subscriber to an event of type {@code T} that should be activated upon the event.
+ * It holds a reference to a {@link EventHandler}.
+ * <p>
+ * It also holds further information about
+ * <ul>
+ *   <li>A non-negative number priority (default is 0). Subscribers are compared based on its priority.
+ *   <li>An array of reified types.
+ *   <li>A name that identifies this subscriber.
+ *   <li>The event type itself (reified).
+ *   <li>The associated contracts to this subscriber.
+ *   <li>Optionally the enclosing type where the event handler is residing (i.e. if the event handler is a Java Method).
+ *   <li>Optionally Pre and Post interceptors specifically for this subscriber.
+ * </ul>
+ * Regarding the comparison, it is only comparable to other subscribers of the same event!
+ *
+ * @author Julijan Katic
+ *
+ * @param <T> The event type
+ */
 public class Subscriber<T> implements Consumer<T>, Disposable, Comparable<Subscriber<T>>, InterceptorInformation {
-	
+
 	private boolean disposed = false;
-	
+
 	private final int priority;
-	private final Class<?>[] reifiedClasses;
+	private final List<Class<?>> reifiedClasses;
 	private final String name;
 	private final EventHandler<? super T> handler;
 	private final Class<?> handlerType;
@@ -42,90 +61,120 @@ public class Subscriber<T> implements Consumer<T>, Disposable, Comparable<Subscr
 		this.postInterceptor = builder.postInterceptor;
 		this.associatedContracts = builder.associatedContracts;
 	}
-	
+
 	@Override
 	public void accept(final T event) throws Exception {
 		final InterceptorInformation preInterceptionInformation = this;
-		
+
 		final InterceptionResult preInterceptionResult = this.preInterceptor
 				.map(preInterceptor -> preInterceptor.apply(preInterceptionInformation, event))
-				.orElseGet(() -> InterceptionResult.success());
-		
+				.orElseGet(InterceptionResult::success);
+
 		if (!this.checkIfCorrectlyReified(event) || !preInterceptionResult.wasSuccessful()) {
 			return;
 		}
-		
+
 		final Result<?> result = this.handler.acceptEvent(event);
-		
+
 
 		// TODO: What to do with that?
 		final InterceptionResult postInterceptionResult = this.postInterceptor
 				.map(postInterceptor -> postInterceptor.apply(preInterceptionInformation, event, result))
-				.orElseGet(() -> InterceptionResult.success());
-		
+				.orElseGet(InterceptionResult::success);
+
 	}
-	
+
 	@Override
 	public void dispose() {
 		if (!disposed) {
 			this.disposed = true;
 		}
 	}
-	
+
 	@Override
 	public boolean isDisposed() {
 		return this.disposed;
 	}
-	
+
 	@Override
 	public int compareTo(final Subscriber<T> other) {
-		return Integer.compare(other.priority, priority);
-	}
-	
-	public void addPreInterceptor(final IPreInterceptor preInterceptor) {
-		if (this.preInterceptor.isPresent()) {
-			final IPreInterceptor currentPreInterceptor = this.preInterceptor.get();
-			
+		if (this.getEventType().equals(other.getEnclosingType())) {
+			return Integer.compare(priority, other.priority);
 		}
+
+		return 0;
 	}
-	
+
+	public void addPreInterceptor(final IPreInterceptor preInterceptor) {
+//		if (this.preInterceptor.isPresent()) {
+//			final IPreInterceptor currentPreInterceptor = this.preInterceptor.get();
+//			if (currentPreInterceptor instanceof CompositeInterceptor) {
+//				final CompositeInterceptor ci = (CompositeInterceptor) currentPreInterceptor;
+//				ci.add(forEvent, preInterceptor);
+//			} else if (preInterceptor instanceof CompositeInterceptor) {
+//				final CompositeInterceptor ci = (CompositeInterceptor) preInterceptor;
+//				ci.add(forEvent, currentPreInterceptor);
+//				//ci.add(forEvent, preInterceptor);
+//				this.preInterceptor = Optional.of(ci);
+//			} else {
+//				final CompositeInterceptor ci = new CompositeInterceptor();
+//				ci.add(forEvent, preInterceptor);
+//				ci.add(forEvent, currentPreInterceptor);
+//				this.preInterceptor = Optional.of(ci);
+//			}
+//		} else {
+//			this.preInterceptor = Optional.of(preInterceptor);
+//		}
+		this.preInterceptor = Optional.of(preInterceptor);
+	}
+
+	public void addPostInterceptor(final IPostInterceptor postInterceptor) {
+		this.postInterceptor = Optional.of(postInterceptor);
+	}
+
 	public int getPriority() {
 		return this.priority;
 	}
-	
-	public Class<?>[] getReifiedClasses() {
+
+	public List<Class<?>> getReifiedClasses() {
 		return this.reifiedClasses;
 	}
 
+	@Override
 	public String getName() {
 		return name;
 	}
 
+	@Override
 	public Class<?> getHandlerType() {
 		return this.handlerType;
 	}
 
+	@Override
 	public Optional<Class<?>> getEnclosingType() {
 		return enclosingType;
 	}
 
+	@Override
 	public Class<T> getEventType() {
 		return forEvent;
 	}
 
+	@Override
 	public List<EventContract> getAssociatedContracts() {
 		return associatedContracts;
 	}
-	
+
 	private boolean checkIfCorrectlyReified(final Object event) {
 		if (event instanceof ReifiedEvent<?>) {
-			if (this.getReifiedClasses() == null || this.getReifiedClasses().length == 0) {
+			if (this.getReifiedClasses() == null || this.getReifiedClasses().isEmpty()) {
 				// We always accept then
 				return true;
 			}
-			
+
 			final ReifiedEvent<?> reifiedEvent = (ReifiedEvent<?>) event;
-			return this.getReifiedClasses()[0].isAssignableFrom(reifiedEvent.getTypeToken().getRawType());
+			return this.reifiedClasses.stream()
+									  .anyMatch(type -> reifiedEvent.getTypeToken().getRawType().equals(type));
 		}
 		// Since it's not a reified event, check is not needed.
 		return true;
@@ -140,13 +189,13 @@ public class Subscriber<T> implements Consumer<T>, Disposable, Comparable<Subscr
 	}
 
 	public static final class Builder<T> {
-		private int priority;
-		private Class<?>[] reifiedClasses;
+		private int priority = 0;
+		private List<Class<?>> reifiedClasses;
 		private String name;
 		private EventHandler<? super T> handler;
 		private Class<?> handlerType = EventHandler.class;
 		private Optional<Class<?>> enclosingType = Optional.empty();
-		private Class<T> forEvent;
+		private final Class<T> forEvent;
 		private Optional<IPreInterceptor> preInterceptor = Optional.empty();
 		private Optional<IPostInterceptor> postInterceptor = Optional.empty();
 		private List<EventContract> associatedContracts = Collections.emptyList();
@@ -155,47 +204,47 @@ public class Subscriber<T> implements Consumer<T>, Disposable, Comparable<Subscr
 			this.forEvent = forEvent;
 		}
 
-		public Builder<T> priority(int priority) {
+		public Builder<T> priority(final int priority) {
 			this.priority = priority;
 			return this;
 		}
 
-		public Builder<T> reifiedClasses(Class<?>[] reifiedClasses) {
-			this.reifiedClasses = reifiedClasses;
+		public Builder<T> reifiedClasses(final Class<?>[] reifiedClasses) {
+			this.reifiedClasses = Arrays.asList(reifiedClasses);
 			return this;
 		}
 
-		public Builder<T> name(String name) {
+		public Builder<T> name(final String name) {
 			this.name = name;
 			return this;
 		}
 
-		public Builder<T> handler(EventHandler<? super T> handler) {
+		public Builder<T> handler(final EventHandler<? super T> handler) {
 			this.handler = handler;
 			return this;
 		}
-		
+
 		public Builder<T> handlerType(final Class<?> handlerType) {
 			this.handlerType = handlerType;
 			return this;
 		}
 
-		public Builder<T> enclosingType(Class<?> enclosingType) {
+		public Builder<T> enclosingType(final Class<?> enclosingType) {
 			this.enclosingType = Optional.ofNullable(enclosingType);
 			return this;
 		}
 
-		public Builder<T> preInterceptor(IPreInterceptor preInterceptor) {
+		public Builder<T> preInterceptor(final IPreInterceptor preInterceptor) {
 			this.preInterceptor = Optional.ofNullable(preInterceptor);
 			return this;
 		}
 
-		public Builder<T> postInterceptor(IPostInterceptor postInterceptor) {
+		public Builder<T> postInterceptor(final IPostInterceptor postInterceptor) {
 			this.postInterceptor = Optional.ofNullable(postInterceptor);
 			return this;
 		}
 
-		public Builder<T> associatedContracts(List<EventContract> associatedContracts) {
+		public Builder<T> associatedContracts(final List<EventContract> associatedContracts) {
 			this.associatedContracts = associatedContracts;
 			return this;
 		}
--- bundles/org.palladiosimulator.analyzer.slingshot.eventdriver/src/org/palladiosimulator/analyzer/slingshot/eventdriver/internal/BusImplementation.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.eventdriver/src/org/palladiosimulator/analyzer/slingshot/eventdriver/internal/BusImplementation.java
@@ -17,14 +17,9 @@ import org.palladiosimulator.analyzer.slingshot.eventdriver.annotations.OnExcept
 import org.palladiosimulator.analyzer.slingshot.eventdriver.annotations.PostIntercept;
 import org.palladiosimulator.analyzer.slingshot.eventdriver.annotations.PreIntercept;
 import org.palladiosimulator.analyzer.slingshot.eventdriver.annotations.Subscribe;
-import org.palladiosimulator.analyzer.slingshot.eventdriver.entity.Subscriber;
 import org.palladiosimulator.analyzer.slingshot.eventdriver.entity.AnnotatedSubscriber;
+import org.palladiosimulator.analyzer.slingshot.eventdriver.entity.Subscriber;
 import org.palladiosimulator.analyzer.slingshot.eventdriver.entity.interceptors.CompositeInterceptor;
-import org.palladiosimulator.analyzer.slingshot.eventdriver.entity.interceptors.CompositePostInterceptor;
-import org.palladiosimulator.analyzer.slingshot.eventdriver.entity.interceptors.CompositePreInterceptor;
-import org.palladiosimulator.analyzer.slingshot.eventdriver.entity.interceptors.IPostInterceptor;
-import org.palladiosimulator.analyzer.slingshot.eventdriver.entity.interceptors.IPreInterceptor;
-import org.palladiosimulator.analyzer.slingshot.eventdriver.entity.interceptors.InterceptorInformation;
 import org.palladiosimulator.analyzer.slingshot.eventdriver.entity.interceptors.PostInterceptor;
 import org.palladiosimulator.analyzer.slingshot.eventdriver.entity.interceptors.PreInterceptor;
 import org.palladiosimulator.analyzer.slingshot.eventdriver.internal.contractchecker.EventContractChecker;
@@ -34,39 +29,46 @@ import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.subjects.PublishSubject;
 import io.reactivex.rxjava3.subjects.Subject;
 
+/**
+ *
+ *
+ * @author Julijan Katic
+ *
+ */
 public final class BusImplementation implements Bus {
-	
+
 	private static final Logger LOGGER = Logger.getLogger(BusImplementation.class);
-	
+
 	private final Subject<Object> bus;
-	
-	/** Maps events (event classes) to handlers */
-	private final Map<Class<?>, CompositeDisposable> observers = new HashMap<>();
-	
+
+	/** Maps IDs to handlers */
+	private final Map<String, CompositeDisposable> observers = new HashMap<>();
+
+	/** The global interceptor */
 	private final CompositeInterceptor compositeInterceptor = new CompositeInterceptor();
-	
+
 	/** Maps exception classes to set of exception handlers */
 	private final Map<Class<?>, Set<Consumer<? super Throwable>>> exceptionHandlers = new HashMap<>();
-	
-	
+
+
 	private final Map<Class<?>, Set<Subscriber<?>>> subscribers = new HashMap<>();
-	
+
 	private boolean registrationOpened = true;
 	private boolean invocationOpened = true;
-	
+
 	private final String identifier;
-	
-	
+
+
 	public BusImplementation(final String identifier) {
 		this.identifier = Objects.requireNonNull(identifier);
 		this.bus = PublishSubject.create();
 		this.init();
 	}
-	
+
 	private void init() {
 		this.register(new EventContractChecker());
 	}
-	
+
 	public BusImplementation() {
 		this("default");
 	}
@@ -75,16 +77,22 @@ public final class BusImplementation implements Bus {
 	public String getIdentifier() {
 		return this.identifier;
 	}
-	
+
+	@Override
 	public <T> void register(final Subscriber<T> subscriber) {
 		if (!this.registrationOpened) {
 			throw new IllegalStateException("This bus has closed registration for new subscribers");
 		}
 		Objects.requireNonNull(subscriber, "Subscriber must not be null!");
-		
-		observers.computeIfAbsent(subscriber.getEnclosingType().orElse(Object.class), cl -> new CompositeDisposable())
+
+		subscriber.addPreInterceptor(compositeInterceptor);
+		subscriber.addPostInterceptor(compositeInterceptor);
+
+		observers.computeIfAbsent(subscriber.getEnclosingType()
+											.map(Class::getName)
+											.orElseGet(subscriber::getName), id -> new CompositeDisposable())
 				 .add(this.bus.ofType(subscriber.getEventType())
-						 	  .doOnNext(ev -> LOGGER.debug("General subscriber called: " + ev.getClass().getSimpleName()))
+						 	  .doOnNext(this::doOnNext)
 						 	  .subscribe(subscriber, this::doError));
 	}
 
@@ -93,26 +101,21 @@ public final class BusImplementation implements Bus {
 		if (!this.registrationOpened) {
 			throw new IllegalStateException("This bus does not income new objects.");
 		}
-		
+
 		Objects.requireNonNull(object, "Observer to register must not be null.");
 		final Class<?> observerClass = object.getClass();
-		
-		if (observers.putIfAbsent(observerClass, new CompositeDisposable()) != null) {
-			LOGGER.warn("Observer has already been registered");
-			//throw new IllegalArgumentException("Observer has already been registered.");
-		}
-		
-		final CompositeDisposable composite = observers.get(observerClass);
-		
+
+		final CompositeDisposable composite = observers.computeIfAbsent(observerClass.getName(), name -> new CompositeDisposable());
+
 		final Set<EventType> events = new HashSet<>();
-		
+
 		System.out.println("Register " + object.getClass().getSimpleName());
-		
+
 		for (final Method method : observerClass.getDeclaredMethods()) {
 			if (method.isBridge() || method.isSynthetic()) {
 				continue;
 			}
-			
+
 			this.searchForSubscribers(composite, events, method, object);
 			this.searchExceptionHandlers(method, object);
 			this.searchPreInterceptors(method, object);
@@ -126,7 +129,7 @@ public final class BusImplementation implements Bus {
 		final CompositeDisposable composite = this.observers.remove(object.getClass());
 		Objects.requireNonNull(composite, "Missing observer; it was not registered before.");
 		composite.dispose();
-		
+
 		final Set<Subscriber<?>> subscribers = this.subscribers.remove(observers.getClass());
 		if (subscribers != null) {
 			subscribers.clear();
@@ -134,62 +137,48 @@ public final class BusImplementation implements Bus {
 	}
 
 	@Override
-	public void post(Object event) {
+	public void post(final Object event) {
 		if (!this.invocationOpened) {
 			throw new IllegalStateException("The bus is not currently allowing posting of events.");
 		}
 		System.out.println("Now post " + event.getClass().getSimpleName());
 		this.bus.onNext(Objects.requireNonNull(event));
 	}
-	
-	private CompositeDisposable getDisposable(final Class<?> event) {
-		// TODO
-		return null;
-	}
 
-	private void searchForSubscribers(final CompositeDisposable composite, 
+	private void searchForSubscribers(final CompositeDisposable composite,
 			final Set<EventType> events, final Method method, final Object object) {
 		if (!method.isAnnotationPresent(Subscribe.class)) {
 			return;
 		}
 		final int modifiers = method.getModifiers();
 		final Subscribe subscribeAnnotation = method.getAnnotation(Subscribe.class);
-		
+
 		if (Modifier.isStatic(modifiers) || !Modifier.isPublic(modifiers)) {
 			throw new IllegalArgumentException("Method " + method.getName() + " has @Subscribe annotation, but is static or is not public.");
 		}
-		
+
 		final Class<?>[] parameterTypes = method.getParameterTypes();
 		if (parameterTypes.length != 1) {
 			throw new IllegalArgumentException("Method " + method.getName() + " has @Subscribe annotation, but has either 0 or more than one parameters.");
 		}
 		final Class<?> eventClass = parameterTypes[0];
 		final EventType eventType = new EventType(eventClass, subscribeAnnotation.reified());
-		
+
 		if (!events.add(eventType)) {
 			throw new IllegalArgumentException("Subscriber for " + eventType.toString() + " has already been registered.");
 		}
-		
+
 		EventContractChecker.checkEventContract(method, object, eventClass);
-		
+
 		final Class<?> returnType = method.getReturnType();
 		if (!returnType.equals(void.class) && !returnType.equals(Void.class) && !returnType.equals(Result.class)) {
 			throw new IllegalArgumentException("Observables must return either void (primitive), Void (object) or Result, but this method returns " + returnType.getSimpleName());
 		}
-		
-		
-		LOGGER.debug("\tAdded subscriber method " + method.getName());
-		composite.add(
-				this.bus.ofType(eventClass)
-						.doOnNext(ev -> System.out.println("ON NEXT " + ev.getClass().getSimpleName()))
-						.subscribe(
-								(Subscriber) AnnotatedSubscriber.fromJavaMethod(eventClass, object, method, subscribeAnnotation, compositeInterceptor, compositeInterceptor).build(),
-								this::doError
-						)
-		);
-		
+
+
+		this.register(AnnotatedSubscriber.fromJavaMethod(eventClass, object, method, subscribeAnnotation, compositeInterceptor, compositeInterceptor).build());
 	}
-	
+
 	private void doError(final Throwable error) {
 		LOGGER.error("An error occurred: " + error.getClass().getSimpleName() + ":: " + error.getMessage());
 		this.exceptionHandlers.keySet().stream()
@@ -197,21 +186,31 @@ public final class BusImplementation implements Bus {
 				.flatMap(exClazz -> this.exceptionHandlers.get(exClazz).stream())
 				.forEach(exHandler -> exHandler.accept(error));
 	}
-	
+
+	/**
+	 * Helper method for debuging purposes. Every time an event happens (no matter what),
+	 * even if an error happened, this method will be called and debugged.
+	 *
+	 * @param event The event that happened
+	 */
+	private void doOnNext(final Object event) {
+		LOGGER.debug("About to call the subscribers for the following events: " + event.getClass().getSimpleName());
+	}
+
 	private void searchPreInterceptors(final Method method, final Object object) {
 		if (!method.isAnnotationPresent(PreIntercept.class)) {
 			return;
 		}
-		
+
 		if (!Modifier.isPublic(method.getModifiers())) {
 			throw new IllegalArgumentException("Method " + method.getName() + " for pre-interception is not public.");
 		}
-		
+
 		final PreInterceptor preInterceptor = new PreInterceptor(method, object);
-		
+
 		this.compositeInterceptor.add(preInterceptor.forEvent(), preInterceptor);
 	}
-	
+
 	private void searchPostInterceptors(final Method method, final Object object) {
 		if (!method.isAnnotationPresent(PostIntercept.class)) {
 			return;
@@ -222,51 +221,52 @@ public final class BusImplementation implements Bus {
 		final PostInterceptor postInterceptor = new PostInterceptor(method, object);
 		this.compositeInterceptor.add(postInterceptor.forEvent(), postInterceptor);
 	}
-	
+
 	private void searchExceptionHandlers(final Method method, final Object target) {
 		if (!method.isAnnotationPresent(OnException.class)) {
 			return;
 		}
 		final Class<?>[] params = method.getParameterTypes();
-		
+
 		final Consumer<? super Throwable> onException;
-		
-		if (params.length == 1) {
-			if (!Throwable.class.isAssignableFrom(params[0])) {
-				throw new IllegalArgumentException("First parameter must be throwable type");
-			}
-			
-			onException = exception -> {
-				try {
-					method.invoke(target, exception);
-				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-					// TODO:
-				}
-			};
-		} else {
+
+		if (params.length != 1) {
 			throw new IllegalArgumentException("");
 		}
-		
+		if (!Throwable.class.isAssignableFrom(params[0])) {
+			throw new IllegalArgumentException("First parameter must be throwable type");
+		}
+
+		onException = exception -> {
+			try {
+				method.invoke(target, exception);
+			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+				// TODO:
+			}
+		};
+
 		this.exceptionHandlers.computeIfAbsent(params[0], eventType -> new HashSet<>())
 							  .add(onException);
 	}
-	
+
+	@Override
 	public void closeRegistration() {
 		this.registrationOpened = false;
 		this.invocationOpened = true;
 	}
-	
+
+	@Override
 	public void acceptEvents(final boolean accept) {
 		if (this.registrationOpened) {
 			return;
 		}
 		this.invocationOpened = accept;
 	}
-	
+
 	public static class EventType {
-		private Class<?> eventClass;
+		private final Class<?> eventClass;
 		private Class<?>[] reification;
-		
+
 		public EventType(final Class<?> eventClass, final Class<?>[] reification) {
 			this.eventClass = eventClass;
 			this.reification = reification;
@@ -285,30 +285,33 @@ public final class BusImplementation implements Bus {
 		}
 
 		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
+		public boolean equals(final Object obj) {
+			if (this == obj) {
 				return true;
-			if (obj == null)
+			}
+			if (obj == null) {
 				return false;
-			if (getClass() != obj.getClass())
+			}
+			if (getClass() != obj.getClass()) {
 				return false;
-			EventType other = (EventType) obj;
+			}
+			final EventType other = (EventType) obj;
 			return Objects.equals(eventClass, other.eventClass) && Arrays.equals(reification, other.reification);
 		}
-		
+
 		@Override
 		public String toString() {
 			final StringBuilder stringBuilder = new StringBuilder();
 			stringBuilder.append("EventType[event = <");
 			stringBuilder.append(eventClass.getName());
 			stringBuilder.append(">, reified = {");
-			
+
 			for (final Class<?> clazz : this.reification) {
 				stringBuilder.append("<");
 				stringBuilder.append(clazz.getName());
 				stringBuilder.append(">");
 			}
-			
+
 			stringBuilder.append("}]");
 			return stringBuilder.toString();
 		}
