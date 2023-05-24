9ba6f8a0318e481124f2cb05f61cfeb972060a6f
Julijan Katic <julijan@katic.io>
2023-05-24 09:42:32 +0200

    Provide MDSDBlackboard as well

--- bundles/org.palladiosimulator.analyzer.slingshot.workflow/src/org/palladiosimulator/analyzer/slingshot/workflow/WorkflowConfigurationModule.java
+++ bundles/org.palladiosimulator.analyzer.slingshot.workflow/src/org/palladiosimulator/analyzer/slingshot/workflow/WorkflowConfigurationModule.java
@@ -1,19 +1,23 @@
 package org.palladiosimulator.analyzer.slingshot.workflow;
 
 import org.palladiosimulator.analyzer.slingshot.core.extension.AbstractSlingshotExtension;
+import org.palladiosimulator.analyzer.slingshot.workflow.jobs.MDSDBlackboardProvider;
 
 import de.uka.ipd.sdq.simucomframework.SimuComConfig;
+import de.uka.ipd.sdq.workflow.mdsd.blackboard.MDSDBlackboard;
 
 public class WorkflowConfigurationModule extends AbstractSlingshotExtension {
 
 	static final SimuComConfigProvider simuComConfigProvider = new SimuComConfigProvider();
-
+	public static final MDSDBlackboardProvider blackboardProvider = new MDSDBlackboardProvider();
+	
 	public WorkflowConfigurationModule() {
 	}
 
 	@Override
 	protected void configure() {
 		bind(SimuComConfig.class).toProvider(simuComConfigProvider);
+		bind(MDSDBlackboard.class).toProvider(blackboardProvider);
 	}
 
 	@Override
---
+++ bundles/org.palladiosimulator.analyzer.slingshot.workflow/src/org/palladiosimulator/analyzer/slingshot/workflow/jobs/MDSDBlackboardProvider.java
@@ -0,0 +1,20 @@
+package org.palladiosimulator.analyzer.slingshot.workflow.jobs;
+
+import javax.inject.Provider;
+
+import de.uka.ipd.sdq.workflow.mdsd.blackboard.MDSDBlackboard;
+
+public class MDSDBlackboardProvider implements Provider<MDSDBlackboard> {
+
+	private MDSDBlackboard blackboard;
+	
+	void set(final MDSDBlackboard blackboard) {
+		this.blackboard = blackboard;
+	}
+	
+	@Override
+	public MDSDBlackboard get() {
+		return this.blackboard;
+	}
+
+}
